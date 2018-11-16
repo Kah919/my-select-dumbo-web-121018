81ccbe509f7dedea0b2f2a2dd4f6c5c54c2c7fce
Kah Yap <kahyap13@gmail.com>
2018-11-16 00:53:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(array)
  i = 0
  new_array = []
   while i < array.size
-    if yield (array[i]) == true
+    if yield(array[i]) == true
       new_array << array[i]
     end
     i += 1
