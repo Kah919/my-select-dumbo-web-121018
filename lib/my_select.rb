7afcb00ca7c64f64d60fbab13ad672977a6ad025
Kah Yap <kahyap13@gmail.com>
2018-11-16 00:50:24 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,24 +1,12 @@
-# def my_select(collection)
-#  # your code here!
-# selected = []
-# i = 0
-#   while i < collection.length
-#     if yield (collection[i]) == true
-#       selected << collection[i]
-#     end
-#     i += 1
-#   end
-# end
-def my_select(array)
-  i = 0
-  new_array = []
-  while i < array.size
-    if yield(array[i]) == true
-      new_array << array[i]
-    else
-      #nothing
+def my_select(collection)
+ # your code here!
+selected = []
+i = 0
+  while i < collection.size
+    if yield (collection[i]) == true
+      selected << collection[i]
     end
     i += 1
   end
-  new_array
 end
+
