be4cc1bf8064de9c47e697a7d03a5b058b6d954b
Kah Yap <kahyap13@gmail.com>
2018-11-16 00:53:12 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(array)
  # your code here!
  i = 0
- selected = []
+ new_array = []
   while i < array.size
     if yield (array[i]) == true
-      selected << array[i]
+      new_array << array[i]
     end
     i += 1
   end
-  return selected
+  return new_array
 end
